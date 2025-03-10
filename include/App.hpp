9cea7a249aba225e7781e4915599ce4f0351fd4a
Kevin171717 <140044307+Kevin171717@users.noreply.github.com>
2025-03-10 13:55:58 +0800

    add char

--- include/App.hpp
+++ include/App.hpp
@@ -3,6 +3,13 @@
 
 #include "pch.hpp" // IWYU pragma: export
 
+#include "Util/Image.hpp"
+#include "Util/GameObject.hpp"
+#include "Util/Renderer.hpp"
+#include "Character.hpp"
+#include "AnimatedCharacter.hpp"
+
+
 class App {
 public:
     enum class State {
@@ -22,8 +29,17 @@ public:
 private:
     void ValidTask();
 
-private:
     State m_CurrentState = State::START;
+
+    // 遊戲根物件，負責管理所有物件
+    Util::GameObject m_Root;
+
+    // 角色物件
+    std::shared_ptr<Character> m_Player;
+    std::shared_ptr<AnimatedCharacter> m_Enemy;
+
+    // 遊戲渲染器
+    std::unique_ptr<Util::Renderer> m_Renderer;
 };
 
 #endif
--- src/App.cpp
+++ src/App.cpp
@@ -7,12 +7,28 @@
 
 void App::Start() {
     LOG_TRACE("Start");
+
+    // 1️⃣ 創建玩家角色 (戰機)
+    m_Player = std::make_shared<Character>(RESOURCE_DIR "/character/test_plane.png");
+    m_Player->SetPosition({-112.5f, -140.5f});  // 螢幕中央
+    m_Player->SetZIndex(10);                  // 讓玩家顯示在最前面
+    m_Root.AddChild(m_Player);
+
+
+
+    // 3️⃣ 初始化 Renderer 並加入根物件
+    m_Renderer = std::make_unique<Util::Renderer>(
+        std::vector<std::shared_ptr<Util::GameObject>>{std::make_shared<Util::GameObject>(m_Root)}
+    );
+
     m_CurrentState = State::UPDATE;
 }
 
 void App::Update() {
     
     //TODO: do your things here and delete this line <3
+    // 更新Renderer以繪製角色物件
+    m_Renderer->Update();
     
     /*
      * Do not touch the code below as they serve the purpose for
